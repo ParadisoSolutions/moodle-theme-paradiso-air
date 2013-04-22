83da11ee5cd78a7145e91d1eb28008956b73d680
Herson Cruz <herson@paradisosolutions.com>
2013-04-22 17:59:41 -0600

    Fixed copyright header for all files

--- lang/en/theme_paradiso_air.php
+++ lang/en/theme_paradiso_air.php
@@ -1,29 +1,13 @@
 <?php
-
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Strings for component 'theme_standard', language 'en', branch 'MOODLE_20_STABLE'
+ * Strings for component 'theme_paradiso_air', language 'en', branch 'MOODLE_20_STABLE'
  *
- * @package   theme_standard
- * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @package   theme_paradiso_air
+ * @copyright 2012 onwards Paradiso Solutions {@link http://paradisosolutions.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'paradiso_air';
+$string['pluginname'] = 'Paradiso Air';
 $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
 $string['choosereadme'] = 'This theme is a very basic white theme, with a minimum amount of CSS added to the base theme to make it actually usable.';
